5d8b1c1c1bd2558d7a2679486d78625b17fb471f
Siddhant <siddhantban02@gmail.com>
2024-06-07 10:56:52 +0530

    Added Frontend and Axios

--- 02_Backend_Frontend/Backend/server.js
+++ 02_Backend_Frontend/Backend/server.js
@@ -5,8 +5,39 @@ const app=express();
 app.get('/', (req,res)=>{
     res.send("Server up");
 })
-let PORT= 3000 ||process.env.PORT;
+
+app.get('/api/jokes', (req,res)=>{
+    const jokes=[
+        {
+            id: 1,
+            title: "joke 1",
+            content: "This is a joke",
+        },
+        {
+            id: 2,
+            title: "joke 2",
+            content: "This is another joke",
+        },
+        {
+            id: 3,
+            title: "joke 3",
+            content: "And this is another joke",
+        },
+        {
+            id: 4,
+            title: "joke 4",
+            content: "This is the 4th joke",
+        },
+        {
+            id: 5,
+            title: "joke 5",
+            content: "This is the last joke, hopefully",
+        }
+    ]
+    res.send(jokes);
+})
+let PORT= process.env.PORT;
 
 app.listen(PORT, ()=>{
-    console.log("Port up and working")
+    console.log(`Port up and working in http://localhost:${PORT}`);
 })
--- 02_Backend_Frontend/Frontend/src/App.jsx
+++ 02_Backend_Frontend/Frontend/src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [jokes, setJokes]=useState([]);
 
-  return (
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((err) => {
+        console.log(err); 
+      });
+  }, []);
+
+  return ( 
     <>
       <h1>BRZRKR</h1>
+      <p>{jokes.length}</p>
+      {
+      jokes.map((joke)=>(
+        <div key={joke.id}>
+          <h1>{joke.title}</h1>
+          <p>{joke.content}</p>
+        </div>
+      ))
+}
     </>
   )
 }
