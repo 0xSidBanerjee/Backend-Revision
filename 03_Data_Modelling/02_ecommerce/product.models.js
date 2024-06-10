6d630600a48aa854331d8c56e3c860d921bedeea
Siddhant <siddhantban02@gmail.com>
2024-06-10 09:50:49 +0530

    Ecommerce Product Schema

---
+++ 03_Data_Modelling/02_ecommerce/product.models.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const productSchema = mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true
+        },
+        price:{
+            type: Number,
+            required: true
+        },
+        description:{
+            type: String,
+            required: true
+        },
+        productImage:{
+            type: String
+        },
+        category:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Category',
+            required: true
+        },
+        productStock:{
+            type: Number,
+            required: true,
+            default: 0
+        },
+        owner:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    }, {timestamps: true}
+)
+
+export const Product = mongoose.model('Product', productSchema)
