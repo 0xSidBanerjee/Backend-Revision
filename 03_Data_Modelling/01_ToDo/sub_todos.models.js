559978827fd9349fbff18f0f03b755b5f5b104d4
Siddhant <siddhantban02@gmail.com>
2024-06-10 09:32:37 +0530

    Ecommerce User Schema

--- 03_Data_Modelling/01_ToDo/sub_todos.models.js
+++ 03_Data_Modelling/01_ToDo/sub_todos.models.js
@@ -1,8 +1,20 @@
-import mongoose from 'mongoose'
+import mongoose, { Mongoose } from 'mongoose'
 
 const sub_todoSchema = new mongoose.Schema(
     {
-
+        title: {
+            type: String,
+            required: true,
+        },
+        complete: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        createdBy: {
+            type: Mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
     }, {timestamps: true})
 
 export const SubTodo = mongoose.model('SubTodo', sub_todoSchema);
